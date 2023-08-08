8f9907ae26d5506843d5b4de38a5b092b29ef1ae
Aisulu Shahabdin <aisulu_sh_98@mail.ru>
2023-08-09 00:37:01 +0600

    changes for hw2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import math
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -12,10 +16,37 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        self.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        # return name
+        if len(name) > 10:
+            self.__name = name[:10]
+        else:
+            self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls, file_path: str = '../src/items.csv'):
+        Item.all.clear()
+        with open(file_path) as csv_file:
+            reader = csv.reader(csv_file)
+            for row in reader:
+                name, price, quantity = row
+                if price.isalpha():
+                    continue
+                Item.all.append(cls(name, price, quantity))
+                # print(len(Item.all))
+
+    @staticmethod
+    def string_to_number(str_num):
+        return math.floor(float(str_num))
 
     def calculate_total_price(self) -> float:
         """
